530a8fa1f1fff4ae156daef87de5202ba23adb12
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-11 00:11:07 +0200

    remove default field name

--- backend/post/models.py
+++ backend/post/models.py
@@ -5,7 +5,7 @@ from user.models import MyUser
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50, blank=False, null=False, default='username', unique=True)
+    title = models.CharField(max_length=50, blank=False, null=False, unique=True)
     user_id = models.ForeignKey(MyUser, blank=True, null=True, on_delete=models.CASCADE)
     company_id = models.ForeignKey(Company, blank=True, null=True, on_delete=models.CASCADE)
     text = models.CharField(max_length=30, blank=False, null=False, unique=True)
