7a9e59c93d2131dc4ad767a298d2c07504c7dbcc
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-10 21:50:14 +0200

    add swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from drf_yasg2 import openapi
+from drf_yasg2.views import get_schema_view
+from rest_framework import permissions
+
+SchemaView = get_schema_view(
+    openapi.Info(
+        title="topnews API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include('user.urls'))
 ]
