03950a4353b37d65d61200cd01683452aab2bc77
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-11 22:25:16 +0200

    fix bug with creating users from django admin panel

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+
 from company.models import Company
 
 
 class MyUser(AbstractUser):
-    first_name = models.CharField('first name', max_length=50, blank=False, null=False, default='username', unique=True)
+    first_name = models.CharField('first name', max_length=50, blank=False, null=False, default='username')
     last_name = models.CharField('last name', max_length=50, blank=False, null=False, default='username')
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
@@ -20,3 +22,13 @@ class MyUser(AbstractUser):
 
     class Meta:
         db_table = 'user'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_name = self.username
+
+    def save(self, *args, **kwargs):
+        if self.username != self.__original_name:
+            self.date_updated = timezone.now()
+        super().save(*args, **kwargs)
+        self.__original_name = self.username
