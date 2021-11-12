a90345696e65bd372320e9339a7f838c8f6c45f1
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-12 14:25:29 +0200

    add url for user details by id

--- backend/rest_api/urls.py
+++ backend/rest_api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from rest_api.views import UsersView, CompaniesView, PostsView, SignUpView
-
+from rest_api.views import UsersView, CompaniesView, PostsView, SignUpView, UserDetailView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/<int:user_id>/', UserDetailView.as_view(), name='user_details'),
     path('user/', UsersView.as_view({'get': 'list'}), name='users_details'),
     path('post/', PostsView.as_view({'get': 'list'}), name='posts_details'),
     path('company/', CompaniesView.as_view({'get': 'list'}), name='companies_details'),
