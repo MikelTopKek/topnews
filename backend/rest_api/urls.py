a5e9e3b074daf51f85739c6ac7c38bc0691b39a1
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-12 15:31:19 +0200

    merge UserViewSet and UserDetailView

--- backend/rest_api/urls.py
+++ backend/rest_api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from rest_api.views import UsersView, CompaniesView, PostsView, SignUpView, UserDetailView
+from rest_api.views import CompaniesView, PostsView, SignUpView, UserViewSet
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/<int:user_id>/', UserDetailView.as_view(), name='user_details'),
-    path('user/', UsersView.as_view({'get': 'list'}), name='users_details'),
+    path('users/', UserViewSet.as_view({'get': 'list'}), name='users_details'),
+    path('users/<int:user_id>/', UserViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='user_details'),
     path('post/', PostsView.as_view({'get': 'list'}), name='posts_details'),
     path('company/', CompaniesView.as_view({'get': 'list'}), name='companies_details'),
 ]
--- backend/rest_api/views/users.py
+++ backend/rest_api/views/users.py
@@ -1,26 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from user.serializers import UserGetSerializer
-from rest_framework.viewsets import ModelViewSet
 
 UserModel = get_user_model()
 
 
-class UserDetailView(APIView):
-    def get(self, request, user_id, *args, **kwargs):
-        user = UserModel.objects.get(id=user_id)
-        serializer = UserGetSerializer(user, context={'request': request})
-        return Response(serializer.data)
-
-
-class UsersView(ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = UserModel.objects.all()
     serializer_class = UserGetSerializer
+    http_method_names = ['get', 'delete']
 
     @action(detail=False, methods=['get'])
     def list(self, request, *args, **kwargs):
         return super().list(request)
 
+    def retrieve(self, request, *args, **kwargs):
+        user = UserModel.objects.get(id=self.kwargs['user_id'])
+        serializer = UserGetSerializer(user, context={'request': request})
+        return Response(serializer.data)
