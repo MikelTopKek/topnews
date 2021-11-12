61def7bcdf63919d27ca12203e7d390414b5868d
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-12 17:38:58 +0200

    create endpoints for companies get, create and delete

--- backend/rest_api/urls.py
+++ backend/rest_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from rest_api.views import CompaniesView, PostsViewSet, SignUpView, UserViewSet
+from rest_api.views import (CompaniesViewSet, PostsViewSet, SignUpView,
+                            UserViewSet)
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -11,5 +12,7 @@ urlpatterns = [
     path('users/<int:user_id>/', UserViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='user_details'),
     path('posts/', PostsViewSet.as_view({'post': 'create', 'get': 'list'}), name='posts_details'),
     path('posts/<post_id>/', PostsViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='posts_details'),
-    path('company/', CompaniesView.as_view({'get': 'list'}), name='companies_details'),
+    path('companies/', CompaniesViewSet.as_view({'post': 'create', 'get': 'list'}), name='companies_details'),
+    path('companies/<company_id>/',
+         CompaniesViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='company_details'),
 ]
--- backend/rest_api/views/companies.py
+++ backend/rest_api/views/companies.py
@@ -1,14 +1,31 @@
+from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from company.models import Company
 from company.serializers import CompanySerializer
 
 
-class CompaniesView(ModelViewSet):
+class CompaniesViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    http_method_names = ['get', 'post', 'delete']
 
     @action(detail=False, methods=['get'])
     def list(self, request, *args, **kwargs):
         return super().list(request)
+
+    def retrieve(self, request, *args, **kwargs):
+        post = Company.objects.get(id=self.kwargs['company_id'])
+        serializer = CompanySerializer(post, context={'request': request})
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['delete'])
+    def destroy(self, request, *args, **kwargs):
+        user = Company.objects.get(id=self.kwargs['company_id'])
+        user.delete()
+        return Response(status.HTTP_200_OK)
+
+    def perform_create(self, serializer):
+        serializer.save()
