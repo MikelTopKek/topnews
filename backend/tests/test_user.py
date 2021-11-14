d802e9e27678647db3cb334c2fa58b404f8363d9
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-14 21:08:12 +0200

    add tests for user

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-USER_MODEL = get_user_model()
+UserModel = get_user_model()
 URL_SIGN_UP = reverse('signup')
+URL_USER = reverse('user_details', kwargs={'user_id': 2})
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_view_sign_up(authenticated_client, data_user):
 
     client = authenticated_client
@@ -15,6 +16,17 @@ def test_view_sign_up(authenticated_client, data_user):
     print(request.json())
 
     assert request.status_code == status.HTTP_201_CREATED
-    recipients_after_post = USER_MODEL.objects.get(id='1')
+    recipients_after_post = UserModel.objects.get(id='2')
 
     assert recipients_after_post is not None
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_user_delete(authenticated_client, data_user, data_valid_sign_up_admin_user):
+
+    client = authenticated_client
+    client.post(URL_SIGN_UP, data_user, format='json')
+    request = client.delete(URL_USER, format='json')
+
+    assert request.status_code == status.HTTP_200_OK
+    assert UserModel.objects.all().count() == 1
