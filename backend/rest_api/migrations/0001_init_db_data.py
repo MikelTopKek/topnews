e9f0fc18d21aca2eb9cda3f9139c80190cd21f6a
MykhailoMaliukov <leniva9.panda@gmail.com>
2021-11-15 12:03:39 +0200

    update data migration and add ignoring for testing

--- backend/rest_api/migrations/0001_init_db_data.py
+++ backend/rest_api/migrations/0001_init_db_data.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 from django.contrib.auth import get_user_model
@@ -5,21 +6,22 @@ from django.db import migrations
 
 from company.models import Company
 from post.models import Post
+from utils.env_utils import load_env
 
 UserModel = get_user_model()
 
 
 def create_super_user():
     user_data = {
-        'username': f'admin',
-        'first_name': f'admin',
-        'last_name': f'admin',
-        'email': f'admin@gmail.com',
+        'username': 'admin',
+        'first_name': 'admin',
+        'last_name': 'admin',
+        'email': 'admin@gmail.com',
         'telephone_number': '+380950000000',
         'password': 'password',
-        'is_superuser': True
     }
-    UserModel.objects.create(**user_data)
+    user = UserModel.objects.create_superuser(**user_data, is_active=True)
+    user.save()
 
 
 def delete_everything():
@@ -31,23 +33,20 @@ def delete_everything():
 def init_db_data(apps, schema_editor):
     """Delete all previous records in db """
     delete_everything()
+    create_super_user()
 
     data = {'number_of_users': 200, 'posts_per_user': 30, 'number_of_companies': 10}
-    post_id = 0
-    user_id = 1
-    company_id = 0
 
-    for company in range(data['number_of_companies']):
-        company_id += 1
+    for company_id in range(data['number_of_companies']):
         company_data = {
             'name': f'name-title{company_id}',
             'url': f'url-text{company_id}',
             'address': f'address-topic{company_id}'
         }
-        Company.objects.create(**company_data)
+        company = Company.objects.create(**company_data)
+        company.save()
 
-    for users in range(int(data['number_of_users'])):
-        user_id += 1
+    for user_id in range(1, data['number_of_users']):
         user_data = {
             'username': f'UserName{user_id}',
             'first_name': f'first_name{user_id}',
@@ -59,16 +58,21 @@ def init_db_data(apps, schema_editor):
             'is_superuser': False,
             'company': Company.objects.get(id=random.randint(1, data['number_of_companies']))
         }
-        UserModel.objects.create(**user_data)
-        for posts in range(random.randint(1, int(data['posts_per_user']))):
-            post_id += 1
+        user = UserModel.objects.create(**user_data)
+        user.save()
+
+        for post_id in range(data['posts_per_user']):
             post_data = {
-                'title': f'Post-title {post_id}',
-                'text': f'Text {post_id}',
-                'topic': f'Topic {post_id}',
+                'title': f'Post-title {user_id}-{post_id}',
+                'text': f'Text {user_id}-{post_id}',
+                'topic': f'Topic {user_id}-{post_id}',
                 'user': UserModel.objects.get(id=user_id)
             }
-            Post.objects.create(**post_data)
+            post = Post.objects.create(**post_data)
+            post.save()
+
+
+load_env()
 
 
 class Migration(migrations.Migration):
@@ -80,4 +84,5 @@ class Migration(migrations.Migration):
         migrations.RunSQL('ALTER SEQUENCE post_id_seq RESTART WITH 1'),
         migrations.RunSQL('ALTER SEQUENCE company_id_seq RESTART WITH 1'),
         migrations.RunPython(init_db_data)
-    ]
+    ] if os.getenv('environment_type') == 'local' else []
+
